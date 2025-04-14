64c66e957f803c2568d3150b493114254ec7dfc4
Arghya <arghyadeepsaha17@gmail.com>
2025-04-14 12:45:45 +0530

    Added loadbalancer service and api call to app in backend

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,10 +1,13 @@
 from flask import Flask
-
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, World!"
 
+@app.route('/callback')
+def callbackend():
+    return requests.get('http://0.0.0.0:5000',timeout=10).text
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
