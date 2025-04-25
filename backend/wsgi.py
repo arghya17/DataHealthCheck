9134e4e65272ed69d0f5e1792a891e300c1138b7
Arghya <arghyadeepsaha17@gmail.com>
2025-04-25 18:11:20 +0530

    added code to run query

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,8 +1,16 @@
-from flask import Flask
+from flask import Flask,jsonify,request,render_template
+from flask_cors import CORS
 import psycopg2
 import os
 import requests
 app = Flask(__name__)
+CORS(app)
+# Load environment variables
+DB_HOST = os.getenv("DB_HOST", "postgres-database-databases")
+DB_PORT = os.getenv("DB_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "admin")
+DB_USER = os.getenv("DB_USER", "admin")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "Arghya1712")
 
 @app.route('/')
 def hello():
@@ -10,16 +18,10 @@ def hello():
 
 @app.route('/callback')
 def callbackend():
-    return requests.get('http://0.0.0.0:5000',timeout=10).text
+    return requests.get('http://data-health-check:5000',timeout=10).text
 
 @app.route('/connect_database')
 def connect_database():
-    DB_HOST = os.getenv("DB_HOST", "postgres-database-databases")
-    DB_PORT = os.getenv("DB_PORT", "5432")
-    DB_NAME = os.getenv("DB_NAME", "admin")
-    DB_USER = os.getenv("DB_USER", "admin")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "Arghya1712")
-
     try:
         # Connect to the PostgreSQL server
         conn = psycopg2.connect(
@@ -44,5 +46,33 @@ def connect_database():
         print("❌ Could not connect to PostgreSQL:", e)
         text=  "❌ Could not connect to PostgreSQL: "+str(e)
     return text
+
+@app.route("/query", methods=["POST"])
+def query():
+    conn = psycopg2.connect(
+            host=DB_HOST,
+            port=DB_PORT,
+            dbname=DB_NAME,
+            user=DB_USER,
+            password=DB_PASSWORD
+        )
+    print("✅ Connected to PostgreSQL!")
+    sql = request.json.get("query")
+    try:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            if cur.description:  # SELECT statement
+                columns = [desc[0] for desc in cur.description]
+                rows = cur.fetchall()
+                return jsonify({"columns": columns, "rows": rows})
+            else:
+                conn.commit()
+                return jsonify({"columns": [], "rows": [["Query executed successfully"]]})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400   
+    
+@app.route("/ui", methods=["GET"])
+def ui():
+    return render_template("index.html")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
